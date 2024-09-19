118bb428e3c2ea4e3f7fa6e545175f1656971fdf
satyam-invensis <satyam.sharma@invensis-tech.com>
2024-09-19 12:42:30 +0530

    Update server.mjs

--- Backend/server.mjs
+++ Backend/server.mjs
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://127.0.0.1:5500',
+    origin: 'https://satyam-invensis.github.io',
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type'],
 }));
